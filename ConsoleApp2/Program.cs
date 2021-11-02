130dfbea843603d9470bc45639dd340b1f05bb46
SNDrogozilov <sdrogozilov@gmail.com>
2021-11-02 21:06:32 +0200

    task3

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -21,7 +21,7 @@ namespace ConsoleApp2
         }
     private static void Task1()
         {
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= 11; i++)
             {
                 Console.WriteLine(i);
             }

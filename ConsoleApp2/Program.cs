93c9639c019ce0bb3f666ad4ee608a462b6b7413
SNDrogozilov <sdrogozilov@gmail.com>
2021-11-03 16:12:11 +0200

    1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,22 +10,6 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            /*double a, b, c;
-            Console.WriteLine($"Введите a: ");
-            a = double.Parse(Console.ReadLine());
-            Console.WriteLine($"Введите b: ");
-            b = double.Parse(Console.ReadLine());
-            Console.WriteLine($"Введите c: ");
-            c = double.Parse(Console.ReadLine());
-            if (a == b || b == c || a == c)
-            {
-                Console.WriteLine("Треугольник равнобедренный");
-            }
-            else
-            {
-                Console.WriteLine("Треугольник не равнобедренный");
-            }
-            Console.ReadKey();*/
             double valuePurchase;
             Console.WriteLine("Введите стоимость покупки: ");
             valuePurchase = double.Parse(Console.ReadLine());

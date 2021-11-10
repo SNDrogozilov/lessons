5ac9d5982545410a329f0fb9e5a134b3688fb4fa
SNDrogozilov <sdrogozilov@gmail.com>
2021-11-10 21:25:52 +0200

    factorial task10

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massive = { 8, 5, 7, 1, 81, 44, 18, 101, -2 };
+            int temp;
+            int temp2;
+            for (int i = 0; i < massive.Length - 1; i++)
+            {
+                for (int j = i + 1; j < massive.Length; j++)
+                {
+                    if (massive[i] > massive[j])
+                    {
+                        temp = massive[i];
+                        massive[i] = massive[j];
+                        massive[j] = temp;
+
+                    }
+                }
+            }
+            Console.WriteLine("Отсортированный массив");
+
+            for (int i = 0; i < massive.Length; i++)
+            {
+                if (massive[0] < massive[8])
+                {
+                    temp2 = massive[0];
+                    massive[0] = massive[8];
+                    massive[8] = temp2;
+
+                }
+                Console.WriteLine(massive[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ newTansk2/newTansk2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newTansk2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list2 = new List<string>();
+            List<string> list = new List<string>() { "New", "New1", "New2" };
+            Console.WriteLine("values list");
+            GetListValues(list);
+            Console.WriteLine("values list2");
+            GetListValues(list2);
+            Console.WriteLine("ListValues in progress");
+            ListValues(list, list2);
+            Console.WriteLine("values list2");
+            GetListValues(list2);
+            Console.ReadKey();
+        }
+        static void GetListValues(List<string> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+        static void ListValues(List<string> fullList, List<string> emptyList)
+        {
+            foreach (string item in fullList)
+            {
+                emptyList.Add(item);
+            }
+        }
+    }
+}

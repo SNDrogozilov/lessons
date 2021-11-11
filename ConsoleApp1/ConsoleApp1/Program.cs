6df78015fd2a99612b896a2d0dc1e691805e7848
Alex_Shaparev <alex3ken@gmail.com>
2021-11-11 22:47:07 +0200

    Array Task2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,9 +9,12 @@ namespace ConsoleApp1
             int[,] numbers = {{3, 4, 7}, {32, 54, 765}, {54, 64, -27}, {33, 40, 78}};
             
             ShowArray(numbers);
+            SumFirstLineArray(numbers);
             
             Console.ReadLine();
         }
+
+        
 
         private static void ShowArray(int[,] arr)
         {
@@ -24,6 +27,27 @@ namespace ConsoleApp1
 
                 Console.WriteLine();
             }
+        }
+        
+        private static void SumFirstLineArray(int[,] arr)
+        {
+            int summa = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                //break значит прервать выполнение, дальше не продолжать и выйти из цикла который сейчас выполняется.
+                
+                //так как "i" указывает на номер строки, а нам нужно найти сумму чисел только первой строки массива
+                // поэтому дальше нам нету смысла продолжать выполнять цикл и нагружать систему. Поэтому мы прерываем выполнение
+                
+                if (i > 0) break;
+                
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    summa += arr[i, j];
+                }
+            }
+
+            Console.WriteLine($"The summa of first line = {summa}");
         }
     }
 }
